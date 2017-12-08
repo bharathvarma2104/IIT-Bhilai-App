a6e7a871ce6d00bd510aba1e216e912f0be00f4f
BharathGottumukkala <bharathvarma21041999@gmail.com>
2017-12-08 14:00:31 +0530

    uncomment

--- IITBhilaiApp/app/src/main/java/com/example/iitbhilaiapp/HomePage.java
+++ IITBhilaiApp/app/src/main/java/com/example/iitbhilaiapp/HomePage.java
@@ -21,7 +21,7 @@ public class HomePage extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
